c9144e2e9e2bcc07bdbbc82959ff1d77055fd26e
uniqueahmerd <ahmerdothman@gmail.com>
2025-06-26 06:27:56 +0100

    cors

--- server.js
+++ server.js
@@ -58,10 +58,10 @@ app.use((req, res) => {
     "http://localhost:5174",
   ];
   const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
+  if (origin && allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", "true");
   }
-  res.header("Access-Control-Allow-Credentials", "true");
   res.status(404).json({ success: false, message: "Not Found" });
 });
 
